4b14d3edad7fa30b2739013e64df6d53c798850b
Prappo Prince <prappo.prince@gmail.com>
2019-10-17 02:37:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { exec } = require('child_process');
+var date = new Date();
+var command = 'git add . && git commit -m "'+date+'" && git push -u origin master';
+console.log(command);
+exec(command, (err, stdout, stderr) => {
+    if (err) {
+        // node couldn't execute the command
+        console.error(`Error: ${stderr}`);
+        return;
+    }
+
+    // the *entire* stdout and stderr (buffered)
+    console.log(`Output : ${stdout} \n`);
+    // console.log(`stderr: ${stderr}`);
+});
